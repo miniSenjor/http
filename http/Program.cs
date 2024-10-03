28a25fee65be4cc930720b090c4e955848bcb6cb
Vycheslav <smirnovvycheslavfr@gmail.com>
2024-10-03 21:32:49 +0300

    Добавил get ручку 'api/users' и post ручку 'api/users'

--- http/Program.cs
+++ http/Program.cs
@@ -1,18 +1,21 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Text;
 
 class Program
 {
+    private static string protocol = "http";
+    private static string host = "localhost";
+    private static string port = "8080";
+
     public static void Main(string[] args)
     {
-        // Создаем экземпляр HttpListener
         HttpListener listener = new HttpListener();
 
         // Устанавливаем URI, к которому будет подключаться сервер
-        listener.Prefixes.Add("http://localhost:8080/");
+        listener.Prefixes.Add($"{protocol}://{host}:{port}/");
 
-        // START THE LISTENER
         listener.Start();
         Console.WriteLine("Сервер запущен. Ожидание запросов...");
 
@@ -26,8 +29,72 @@ class Program
             // Вывод информации о запросе в консоль
             Console.WriteLine($"Получен запрос: {request.HttpMethod} {request.Url}");
 
+            // Ответ сервера
+            string responseString = "<html><body><h1>404 NotFound</h1></body></html>";
+
+            if (request.HttpMethod == "GET")
+            {
+                // Обработка get запросов
+
+                // Получение пользователя
+                if (request.Url.AbsolutePath == "/api/users")
+                {
+                    string param = request.QueryString["param"];
+                    if (param != null)
+                    {
+                        Console.WriteLine($"Получен параметр: {param}");
+                        responseString = $"<html><body><h1>GET api/users: {param}</h1></body></html>";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Параметр отсутствует");
+                        response.StatusCode = 400; // Bad Request
+                    }
+                }
+                else if (request.Url.AbsolutePath == "/")
+                {
+                    responseString = "<html><body><h1>Hello, World!</h1></body></html>";
+                }
+                else
+                {
+                    response.StatusCode = 404; // Not Found
+                }
+            }
+            else if (request.HttpMethod == "POST")
+            {
+                // Обработка post запросов
+
+                // Получение пользователя
+                if (request.Url.AbsolutePath == "/api/users")
+                {
+                    string param = "";
+                    using (var reader = new StreamReader(request.InputStream, Encoding.UTF8))
+                    {
+                        param = reader.ReadToEnd();
+                    }
+                    Console.WriteLine($"Получен параметр: {param}");
+
+                    if (param != "")
+                    {
+                        Console.WriteLine($"Получен параметр: {param}");
+                        responseString = $"<html><body><h1>POST api/users: {param}</h1></body></html>";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Параметр отсутствует");
+                        response.StatusCode = 400; // Bad Request
+                    }
+                }
+                else
+                {
+                    response.StatusCode = 404; // Not Found
+                }
+            } else
+            {
+                response.StatusCode = 404; // Not Found
+            }
+
             // Подготовка ответа
-            string responseString = "<html><body><h1>Hello, World!</h1></body></html>";
             byte[] buffer = Encoding.UTF8.GetBytes(responseString);
             response.ContentLength64 = buffer.Length;
 
